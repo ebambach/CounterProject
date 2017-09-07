9a9ac880924d92ae081c0fb0568bc83a181b1687
ebambach <31412533+ebambach@users.noreply.github.com>
2017-09-07 11:49:53 -0400

    initial commit of "for" exercise

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject {
+	class Program {
+		void Calculate() {
+			for (int Counter = 2, Number = 1; Counter <= 10; Counter++) {
+				Number *= Counter;
+				Console.WriteLine($"The factorial of {Counter} is {Number}.");
+			}
+		}
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+		void Run() {
+			Calculate();
+		}
+	}
+}
